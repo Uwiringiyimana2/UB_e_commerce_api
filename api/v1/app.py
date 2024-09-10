9a8464781baca737b43c3a7a5fa09f1ccb0c327e
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-09-10 14:23:00 -0700

    create some api

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" flssk app server
+"""
+from flask import Flask
+from flask_cors import CORS
+from api.v1.views import app_views
+from api.v1.views import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.register_blueprint(app_views)
+CORS(app, resource={r"/api/v1/*": {"origins": "*"}})
+
+
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" some helpfull functions
+"""
+from functools import wraps
+from flask import jsonify, request
+from api.v1.config import Config
+import bcrypt
+import jwt
+from models.db import DB
+from models.user import User
+
+
+db = DB()
+
+
+def hashpassword(passwd):
+    """ hashing password
+    """
+    password = passwd.encode("utf-8")
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed
+
+def token_required(f):
+    """ verify token """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        """decorator"""
+        token = None
+        if "x-access-token" in request.headers:
+            token = request.headers.get("x-access-token")
+        if not token:
+            return jsonify({"error": "Token is missing!"}), 401
+
+        try:
+            data = jwt.decode(token, Config.SECRET_KEY, algorithms=["HS256"])
+            current_user = db.get(User, email=data["email"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message": "Token has expired"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid token!"}), 401
+        except:
+            return jsonify({"error": str(e)}), 401
+        return f(current_user, *args, **kwargs)
+    return decorated
