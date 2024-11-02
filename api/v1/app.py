b78d96f638226359d87bb1095425993762c59fef
ERIC UWIRINGIYIMANA <uwiringiyimanaericmax2000@gmail.com>
2024-11-02 18:42:13 +0200

    removed port to listen to

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,17 @@ from flask_cors import CORS
 from api.v1.views import app_views
 from api.v1.views import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config.from_object(Config)
 app.register_blueprint(app_views)
 CORS(app, resource={r"/api/v1/*": {"origins": "*"}})
+
+
+@app.after_request
+def ensure_pretty_print(response):
+    if not app.debug:
+        response.direct_passthrough = False
+    return response
 
 
 @app.route('/')
@@ -18,4 +25,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
